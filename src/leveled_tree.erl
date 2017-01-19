860a8a95e3f2e04abea19f9f14d8bbdee9693404
Martin Sumner <martin.sumner@adaptip.co.uk>
2017-01-19 22:59:53 +0000

    Width of 32 may be better
    With 4000 keys - build time dropped significantly

--- src/leveled_tree.erl
+++ src/leveled_tree.erl
@@ -25,7 +25,7 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
--define(SKIP_WIDTH, 16).
+-define(SKIP_WIDTH, 32).
 
 
 %%%============================================================================
