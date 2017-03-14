4b60c0e35bc099bbf8c5030ae708898032de33c4
martinsumner <martin.sumner@adaptip.co.uk>
2017-03-14 01:18:50 +0000

    Scan width semi-reverted
    No evidence from valume test that the scan width has made a posiitve
    difference - so reverting, but not fully as slots may now be twice as
    big, so sticking to half previous value

--- src/leveled_sst.erl
+++ src/leveled_sst.erl
@@ -71,7 +71,7 @@
 -define(COMPRESSION_LEVEL, 1).
 -define(BINARY_SETTINGS, [{compressed, ?COMPRESSION_LEVEL}]).
 % -define(LEVEL_BLOOM_BITS, [{0, 8}, {1, 10}, {2, 8}, {default, 6}]).
--define(MERGE_SCANWIDTH, 4).
+-define(MERGE_SCANWIDTH, 8).
 -define(DISCARD_EXT, ".discarded").
 -define(DELETE_TIMEOUT, 10000).
 -define(TREE_TYPE, idxt).
