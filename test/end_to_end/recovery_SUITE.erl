0185b3aa7d069ede1b1ea6ac727e488165c0c95d
Martin Sumner <martin.sumner@adaptip.co.uk>
2018-05-02 00:23:26 +0100

    Fix Accumulators
    Correct inefficient list accumulators

--- test/end_to_end/recovery_SUITE.erl
+++ test/end_to_end/recovery_SUITE.erl
@@ -175,7 +175,7 @@ aae_bustedjournal(_Config) ->
     testutil:corrupt_journal(RootPath, HeadF, 1000, 2048, 1000),
     {ok, Bookie2} = leveled_bookie:book_start(StartOpts),
     
-    FoldKeysFun = fun(B, K, Acc) -> Acc ++ [{B, K}] end,
+    FoldKeysFun = fun(B, K, Acc) -> [{B, K}|Acc] end,
     AllKeyQuery = {keylist, o_rkv, {FoldKeysFun, []}},
     {async, KeyF} = leveled_bookie:book_returnfolder(Bookie2, AllKeyQuery),
     KeyList = KeyF(),
