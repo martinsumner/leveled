0739264a10bb3321bef7bc77b30d4741a7b1626a
Ulf Wiger <ulf@wiger.net>
2018-11-29 15:47:49 +0100

    Add skeleton _app and _sup modules

---
+++ src/leveled_sup.erl
@@ -0,0 +1,23 @@
+-module(leveled_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+%% ===================================================================
+%% API functions
+%% ===================================================================
+
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+%% ===================================================================
+%% Supervisor callbacks
+%% ===================================================================
+
+init([]) ->
+    {ok, { {one_for_one, 5, 10}, []} }.
