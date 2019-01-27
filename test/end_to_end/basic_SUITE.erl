e3bd83179a656726e774ded3bf9863d8eaad47a0
Martin Sumner <martin.sumner@adaptip.co.uk>
2019-01-27 23:31:44 +0000

    Uncomment tests!

--- test/end_to_end/basic_SUITE.erl
+++ test/end_to_end/basic_SUITE.erl
@@ -18,16 +18,16 @@
             ]).
 
 all() -> [
-            % simple_put_fetch_head_delete,
-            % many_put_fetch_head,
-            % journal_compaction,
-            % fetchput_snapshot,
-            % load_and_count,
-            % load_and_count_withdelete,
-            % space_clear_ondelete,
-            % is_empty_test,
-            % many_put_fetch_switchcompression,
-            % bigjournal_littlejournal,
+            simple_put_fetch_head_delete,
+            many_put_fetch_head,
+            journal_compaction,
+            fetchput_snapshot,
+            load_and_count,
+            load_and_count_withdelete,
+            space_clear_ondelete,
+            is_empty_test,
+            many_put_fetch_switchcompression,
+            bigjournal_littlejournal,
             bigsst_littlesst,
             safereaderror_startup,
             remove_journal_test
