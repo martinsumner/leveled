c492c0bd74dc2292a4487ff375e447dbffe38bb3
Martin Sumner <martin.sumner@adaptip.co.uk>
2024-01-04 15:15:21 +0000

    Make bloom fixed FPR
    The previous implementation restricted sizes to factors of 2 (then multiplied by 512 bytes).  There was no reason to be limited to factors of 2 though.
    
    This implementation is backwards compatible, it will read a bloom filter still of any size - but will right-size the filter close to the target fpr of 5% when building a new filter.
    
    Comments now added, with further clarification, and the log output changed to make it clear what the timings relate to.
    
    On a 2021 M1 - check times are now 4 hundredths of a microsecond:
    
    Test with size 250000 has microsecond timings: - build in 59545 then 0.034 per pos-check, 0.039 per neg-check, fpr 0.379 with bits-per-key 0.131
    Test with size 80000 has microsecond timings: - build in 20259 then 0.036 per pos-check, 0.036 per neg-check, fpr 0.069 with bits-per-key 0.819
    Test with size 60000 has microsecond timings: - build in 11874 then 0.033 per pos-check, 0.034 per neg-check, fpr 0.053 with bits-per-key 0.998
    Test with size 40000 has microsecond timings: - build in 7869 then 0.043 per pos-check, 0.034 per neg-check, fpr 0.051 with bits-per-key 0.998
    Test with size 32768 has microsecond timings: - build in 6741 then 0.044 per pos-check, 0.039 per neg-check, fpr 0.049 with bits-per-key 1.000
    Test with size 20000 has microsecond timings: - build in 5399 then 0.037 per pos-check, 0.032 per neg-check, fpr 0.051 with bits-per-key 0.992
    Test with size 10000 has microsecond timings: - build in 1591 then 0.032 per pos-check, 0.032 per neg-check, fpr 0.049 with bits-per-key 0.973
    Test with size 5000 has microsecond timings: - build in 748 then 0.036 per pos-check, 0.036 per neg-check, fpr 0.058 with bits-per-key 0.922
    Test with size 2000 has microsecond timings: - build in 277 then 0.032 per pos-check, 0.032 per neg-check, fpr 0.071 with bits-per-key 0.768
    Test with size 1000 has microsecond timings: - build in 137 then 0.035 per pos-check, 0.032 per neg-check, fpr 0.049 with bits-per-key 1.024

--- src/leveled_ebloom.erl
+++ src/leveled_ebloom.erl
@@ -1,11 +1,20 @@
 %% -------- TinyBloom ---------
 %%
-%% A fixed size bloom that supports 32K keys only, made to try and minimise
-%% the cost of producing the bloom
-%%
+%% A 1-byte per key bloom filter with a 5% fpr.  Pre-prepared segment hashes
+%% (a leveled codec type) are, used for building and checking - the filter
+%% splits a single hash into a 1 byte slot identifier, and 2 x 12 bit hashes
+%% (so k=2, although only a single hash is used).
+%% 
+%% The filter is designed to support a maximum of 64K keys, larger numbers of
+%% keys will see higher fprs - with a 40% fpr at 250K keys.
+%% 
+%% The filter uses the second "Extra Hash" part of the segment-hash to ensure
+%% no overlap of fpr with the leveled_sst find_pos function.
+%% 
+%% The completed bloom is a binary - to minimise the cost of copying between
+%% processes and holding in memory.
 
 -module(leveled_ebloom).
-
 
 -export([
     create_bloom/1,
@@ -33,24 +42,17 @@
 
 -spec create_bloom(list(leveled_codec:segment_hash())) -> bloom().
 %% @doc
-%% Create a binary bloom filter from a list of hashes
+%% Create a binary bloom filter from a list of hashes.  In the leveled
+%% implementation the hashes are leveled_codec:segment_hash/0 type, but only
+%% a single 32-bit hash (the second element of the tuple is actually used in
+%% the building of the bloom filter
 create_bloom(HashList) ->
     SlotCount =
         case length(HashList) of
             0 ->
                 0;
-            L when L > 32768 ->
-                64;
-            L when L > 16284 ->
-                32;
-            L when L > 8192 ->
-                16;
-            L when L > 4096 ->
-                8;
-            L when L > 4096 ->
-                4;
-            _L ->
-                2
+            L ->
+                min(128, max(2, (L - 1) div 512))
         end,
     SlotHashes =
         map_hashes(
@@ -61,10 +63,10 @@ create_bloom(HashList) ->
         ),
     build_bloom(SlotHashes, SlotCount).
 
-
 -spec check_hash(leveled_codec:segment_hash(), bloom()) -> boolean().
 %% @doc
-%% Check for the presence of a given hash within a bloom
+%% Check for the presence of a given hash within a bloom. Only the second
+%% element of the leveled_codec:segment_hash/0 type is used - a 32-bit hash.
 check_hash(_Hash, <<>>) ->
     false;
 check_hash({_SegHash, Hash}, BloomBin) when is_binary(BloomBin)->
@@ -82,7 +84,9 @@ check_hash({_SegHash, Hash}, BloomBin) when is_binary(BloomBin)->
 %%% Internal Functions
 %%%============================================================================
 
--type slot_count() :: 0|2|4|8|16|32|64.
+-type slot_count() :: 0|2..128.
+-type bloom_hash() :: 0..16#FFF.
+-type external_hash() :: 0..16#FFFFFFFF.
 
 -spec map_hashes(
         list(leveled_codec:segment_hash()), tuple(), slot_count()) -> tuple().
@@ -96,8 +100,8 @@ map_hashes([Hash|Rest], HashListTuple, SlotCount) ->
         setelement(Slot + 1, HashListTuple, Hashes ++ SlotHL),
         SlotCount).
 
--spec split_hash(0..16#FFFFFFFF, slot_count())
-        -> {non_neg_integer(), [0..16#FFF]}.
+-spec split_hash(external_hash(), slot_count())
+        -> {non_neg_integer(), [bloom_hash()]}.
 split_hash(Hash, SlotSplit) ->
     Slot = (Hash band 255) rem SlotSplit,
     H0 = (Hash bsr 8) band ?SPLIT_BAND,
@@ -108,9 +112,6 @@ split_hash(Hash, SlotSplit) ->
 match_hash(BloomBin, Pos, Hash) ->
     <<_Pre:Pos/binary, CheckInt:8/integer, _Rest/binary>> = BloomBin,
     (CheckInt bsr Hash) band 1 == 1.
-
-%% This looks ugly and clunky, but in tests it was quicker than modifying an
-%% Erlang term like an array as it is passed around the loop
 
 -spec build_bloom(tuple(), slot_count()) -> bloom().
 build_bloom(_SlotHashes, 0) ->
@@ -128,6 +129,12 @@ build_bloom(SlotHashes, SlotCount) when SlotCount > 0 ->
         lists:seq(1, SlotCount)
     ).
 
+-spec add_hashlist(
+        list(bloom_hash()),
+        non_neg_integer(),
+        non_neg_integer(),
+        0..?INTEGER_SLICES,
+        binary()) -> bloom().
 add_hashlist([], ThisSlice, SliceCount, SliceCount, AccBin) ->
     <<ThisSlice:?INTEGER_SLICE_SIZE/integer, AccBin/binary>>;
 add_hashlist([], ThisSlice, SliceNumber, SliceCount, AccBin) ->
@@ -159,11 +166,7 @@ add_hashlist(Rest, ThisSlice, SliceNumber, SliceCount, AccBin) ->
 -include_lib("eunit/include/eunit.hrl").
 
 generate_orderedkeys(Seqn, Count, BucketRangeLow, BucketRangeHigh) ->
-    generate_orderedkeys(Seqn,
-                            Count,
-                            [],
-                            BucketRangeLow,
-                            BucketRangeHigh).
+    generate_orderedkeys(Seqn, Count, [], BucketRangeLow, BucketRangeHigh).
 
 generate_orderedkeys(_Seqn, 0, Acc, _BucketLow, _BucketHigh) ->
     Acc;
@@ -173,17 +176,12 @@ generate_orderedkeys(Seqn, Count, Acc, BucketLow, BucketHigh) ->
         io_lib:format("K~4..0B", [BucketLow + BNumber]),
     KeyExt =
         io_lib:format("K~8..0B", [Seqn * 100 + leveled_rand:uniform(100)]),
-    
     LK = leveled_codec:to_ledgerkey("Bucket" ++ BucketExt, "Key" ++ KeyExt, o),
     Chunk = leveled_rand:rand_bytes(16),
     {_B, _K, MV, _H, _LMs} =
         leveled_codec:generate_ledgerkv(LK, Seqn, Chunk, 64, infinity),
-    generate_orderedkeys(Seqn + 1,
-                        Count - 1,
-                        [{LK, MV}|Acc],
-                        BucketLow,
-                        BucketHigh).
-
+    generate_orderedkeys(
+        Seqn + 1, Count - 1, [{LK, MV}|Acc], BucketLow, BucketHigh).
 
 get_hashlist(N) ->
     KVL = generate_orderedkeys(1, N, 1, 20),
@@ -212,7 +210,6 @@ check_neg_hashes(BloomBin, HashList, Counters) ->
         end,
     lists:foldl(CheckFun, Counters, HashList).
 
-
 empty_bloom_test() ->
     BloomBin0 = create_bloom([]),
     ?assertMatch(
@@ -222,6 +219,7 @@ bloom_test_() ->
     {timeout, 120, fun bloom_test_ranges/0}.
 
 bloom_test_ranges() ->
+    test_bloom(250000, 2),
     test_bloom(80000, 4),
     test_bloom(60000, 4),
     test_bloom(40000, 4),
@@ -229,7 +227,8 @@ bloom_test_ranges() ->
     test_bloom(20000, 4),
     test_bloom(10000, 4),
     test_bloom(5000, 4),
-    test_bloom(2000, 4).
+    test_bloom(2000, 4),
+    test_bloom(1000, 4).
 
 test_bloom(N, Runs) ->
     ListOfHashLists = 
@@ -251,36 +250,44 @@ test_bloom(N, Runs) ->
 
     SWa = os:timestamp(),
     ListOfBlooms =
-        lists:map(fun({HL, _ML}) -> create_bloom(HL) end, 
-                    SplitListOfHashLists),
+        lists:map(
+            fun({HL, _ML}) -> create_bloom(HL) end, SplitListOfHashLists),
     TSa = timer:now_diff(os:timestamp(), SWa)/Runs,
     
     SWb = os:timestamp(),
-    lists:foreach(fun(Nth) ->
-                        {HL, _ML} = lists:nth(Nth, SplitListOfHashLists),
-                        BB = lists:nth(Nth, ListOfBlooms),
-                        check_all_hashes(BB, HL)
-                     end,
-                     lists:seq(1, Runs)),
-    TSb = timer:now_diff(os:timestamp(), SWb)/Runs,
-    
+    PosChecks =
+        lists:foldl(
+            fun(Nth, ChecksMade) ->
+                {HL, _ML} = lists:nth(Nth, SplitListOfHashLists),
+                BB = lists:nth(Nth, ListOfBlooms),
+                check_all_hashes(BB, HL),
+                ChecksMade + length(HL)
+            end,
+            0,
+            lists:seq(1, Runs)),
+    TSb = timer:now_diff(os:timestamp(), SWb),
+
     SWc = os:timestamp(),
     {Pos, Neg} = 
-        lists:foldl(fun(Nth, Acc) ->
-                            {_HL, ML} = lists:nth(Nth, SplitListOfHashLists),
-                            BB = lists:nth(Nth, ListOfBlooms),
-                            check_neg_hashes(BB, ML, Acc)
-                        end,
-                        {0, 0},
-                        lists:seq(1, Runs)),
+        lists:foldl(
+            fun(Nth, Acc) ->
+                {_HL, ML} = lists:nth(Nth, SplitListOfHashLists),
+                BB = lists:nth(Nth, ListOfBlooms),
+                check_neg_hashes(BB, ML, Acc)
+            end,
+            {0, 0},
+            lists:seq(1, Runs)),
     FPR = Pos / (Pos + Neg),
-    TSc = timer:now_diff(os:timestamp(), SWc)/Runs,
+    TSc = timer:now_diff(os:timestamp(), SWc),
+
+    BytesPerKey =
+        (lists:sum(lists:map(fun byte_size/1, ListOfBlooms)) div 4) / N,
 
     io:format(
         user,
-        "Test with size ~w has microsecond timings: -"
-            " build ~w check ~w neg_check ~w and fpr ~w~n",
-        [N, TSa, TSb, TSc, FPR]).
-
+        "Test with size ~w has microsecond timings: - "
+            "build in ~w then ~.3f per pos-check, ~.3f per neg-check, "
+            "fpr ~.3f with bits-per-key ~.3f~n",
+        [N, round(TSa), TSb / PosChecks, TSc / (Pos + Neg), FPR, BytesPerKey]).
 
 -endif.
