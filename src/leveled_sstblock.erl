ca95b92ec80a7c16686fe227e93f9bd8f13e35c6
Martin Sumner <martin.sumner@adaptip.co.uk>
2025-01-25 16:04:10 +0000

    Experiment with avoiding append

--- src/leveled_sstblock.erl
+++ src/leveled_sstblock.erl
@@ -542,11 +542,24 @@ get_all_block(Type, TypedBlock, PM)
         MBn:MSz/binary,
         RBn:RSz/binary
     >> = decompress_block(CompressedBin, PM),
-    leveled_sst:append(
-        binary_to_term(LBn),
-        binary_to_term(MBn),
-        binary_to_term(RBn)
-    );
+    [
+        L1, L2, L3, L4, L5, L6, L7, L8, L9, L10, L11, L12,
+        L13, L14, L15, L16, L17, L18, L19, L20, L21, L22, L23, L24
+    ] = binary_to_term(LBn),
+    [
+        M1, M2, M3, M4, M5, M6, M7, M8
+    ] = binary_to_term(MBn),
+    [
+        R1, R2, R3, R4, R5, R6, R7, R8, R9, R10, R11, R12,
+        R13, R14, R15, R16, R17, R18, R19, R20, R21, R22, R23, R24
+    ] = binary_to_term(RBn),
+    [
+        L1, L2, L3, L4, L5, L6, L7, L8, L9, L10, L11, L12,
+        L13, L14, L15, L16, L17, L18, L19, L20, L21, L22, L23, L24,
+        M1, M2, M3, M4, M5, M6, M7, M8,
+        R1, R2, R3, R4, R5, R6, R7, R8, R9, R10, R11, R12,
+        R13, R14, R15, R16, R17, R18, R19, R20, R21, R22, R23, R24
+    ];
 get_all_block(_Type, TypedBlock, PM)
         when PM == lz4; PM == zstd ->
     <<
